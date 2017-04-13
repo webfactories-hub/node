7a384d63c08b124045191978bd4a44e8f0a4ce3b
anuroop <anuroopmohan007@hotmail.com>
2017-04-13 21:19:57 +0530

    initial

--- build/build/hello_server.js
+++ build/build/hello_server.js
@@ -1,9 +1,8 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var http = require("http");
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World alwas cool n peaceful!\n');
+    response.end('My first Node app hosted in Amazon Web Service\n');
 });
 var port = 8000;
 server.listen(port);
--- build/hello_server.ts
+++ build/hello_server.ts
@@ -2,7 +2,7 @@ import * as http from 'http';
 
 const server = http.createServer((request, response)=> {
 response.writeHead(200, {'Content-Type': 'text/plain'});
-response.end('Hello World alwas cool n peaceful!\n');
+response.end('My first Node app hosted in Amazon Web Service\n');
 
 });
 
