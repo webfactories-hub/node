fe6144a2fddc9d1188bd34fac97e0b4d777485db
anuroop <anuroopmohan007@hotmail.com>
2017-04-13 21:22:35 +0530

    initial

--- build/build/hello_server.js
+++ build/build/hello_server.js
@@ -2,7 +2,7 @@
 var http = require("http");
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('My first Node app hosted in Amazon Web Service\n');
+    response.end('My first Node app hosted in Amazon Web Services\n');
 });
 var port = 8000;
 server.listen(port);
--- build/hello_server.ts
+++ build/hello_server.ts
@@ -2,7 +2,7 @@ import * as http from 'http';
 
 const server = http.createServer((request, response)=> {
 response.writeHead(200, {'Content-Type': 'text/plain'});
-response.end('My first Node app hosted in Amazon Web Service\n');
+response.end('My first Node app hosted in Amazon Web Services\n');
 
 });
 
